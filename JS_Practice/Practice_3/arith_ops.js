49e587d46ed91eb6283d5431dd3f252f9e059156
Prince Malviya <pm306616@gmail.com>
2025-10-22 13:53:48 +0530

    learn arithmetic operators

---
+++ JS_Practice/Practice_3/arith_ops.js
@@ -0,0 +1,33 @@
+// Learn all the arithmetic operators
+
+let a = 10;
+
+a = a + 10;
+console.log(a); // 20
+
+a = a - 5;
+console.log(a); // 15
+
+a = a * 5;
+console.log(a); // 75
+
+a = a / 5;
+console.log(a); //15
+
+a = a % 4; 
+console.log(a); //3
+
+a = a ** 2; 
+console.log(a); //9
+
+a++;    // increment operator
+console.log(a); //10
+
+a--;    // decrement operator
+console.log(a); //9
+
+console.log(a++); //9   prints first then increment
+console.log(a); //10   prints incremented value
+
+console.log(a--); //10   prints first then decrement
+console.log(a); //9    prints decremented value
