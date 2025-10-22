dc9a775d24058a24595f4086e27808b8e03ad9f5
Prince Malviya <pm306616@gmail.com>
2025-10-22 14:07:38 +0530

    learn assignment operators

---
+++ JS_Practice/Practice_3/assign_ops.js
@@ -0,0 +1,21 @@
+// learn assignment operators
+
+let a = 10;  // "=" assignment operator
+
+a += 5; //increments a by 5
+console.log(a);  //15
+
+a -= 5; //decrement a by 5
+console.log(a);  //10
+
+a *= 5; //multiply a by 5 and store in a itself
+console.log(a);  //50
+
+a /= 5; //divide a by 5 and store in a itself
+console.log(a);  //10
+
+a %= 4; //a modulo 4 and store in a itself
+console.log(a);  //2
+
+a **= 4; //a to the power 4 and store in a itself
+console.log(a);  //16
