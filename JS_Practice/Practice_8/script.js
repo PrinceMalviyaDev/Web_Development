44a05762c9c7bd15a296692d60647ce153cb6163
Hariom-Malviya <hariommalviya240801@gmail.com>
2025-10-30 20:44:41 +0530

    learn DOM manipulation

--- JS_Practice/Practice_8/script.js
+++ JS_Practice/Practice_8/script.js
@@ -9,23 +9,23 @@ console.log(document.querySelector(".container").innerHTML);  // returns the htm
 
 console.log(document.querySelector(".container").innerText);  //"Hey I am a box."(for one div child), and "Hey I am a box.\nHeyI am a box."(for 2 div childs with same texts)
 
-console.log(document.querySelector(".container").outerHTML);  // returns the container code as well as inside childs also in text format
+console.log(document.querySelector(".container").outerHTML);  // returns the container code as well as inner childs as well in text format.
 
 
 
-console.log(document.querySelector(".container").tagName);
+console.log(document.querySelector(".container").tagName);   //for element childs only
 
-console.log(document.querySelector(".container").nodeName);
+console.log(document.querySelector(".container").nodeName);   //for element as well as text and comment childs as well.
 
 
-console.log(document.querySelector(".container").textContent);
+console.log(document.querySelector(".container").textContent);  //returns all the text content inside the container including tab spaces and endlines too.
 
 
 console.log(document.querySelector(".container").hidden);  //returns false because the container is not hidden.
 
 document.querySelector(".container").hidden = true;  // It hides the element.
 
-document.querySelector(".container").hidden = false;  // Unhide the element.
+document.querySelector(".container").hidden = false;  // Unhides the element.
 
 
 document.querySelector(".box").innerHTML = "Hey I am Prince.";   //changes the inner HTML of first box.
---
+++ JS_Practice/Practice_8/script4.js
@@ -0,0 +1,16 @@
+// document.querySelector(".created").remove();
+// document.querySelector(".created").remove();
+
+
+console.log(document.querySelector(".container").className);   //returns all the class names.
+
+console.log(document.querySelector(".container").classList);   //returns the list of class names
+
+document.querySelector(".container").classList.add("prince");   //adds a  class named "prince" to the container.
+
+document.querySelector(".container").classList.remove("prince");  //removed the class named "prince" from the container.
+
+
+document.querySelector(".container").classList.toggle("red");  //toggled the class "red" i.e., means it has deactivated the "red" class.
+
+document.querySelector(".container").classList.toggle("red");  //toggled the class "red" i.e., means it has reactivated the "red" class.
