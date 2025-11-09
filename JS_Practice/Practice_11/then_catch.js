34d5f69301a4f0c38a95b810f63c10e923b9ab97
PrinceMalviyaDev <pm306616@gmail.com>
2025-11-09 22:05:15 +0530

    learn use of then and catch in promises

---
+++ JS_Practice/Practice_11/then_catch.js
@@ -0,0 +1,17 @@
+const getPromise = () => {
+    return new Promise((resolve, reject) => {
+        console.log("I am a Promise.");
+        // resolve("success");
+        reject("network error!");
+    })
+}
+
+let promise = getPromise();
+
+promise.then((res) => {
+    console.log("promise fulfilled.", res)
+});
+
+promise.catch((err) => {
+    console.log("Rejected,", err);
+});
