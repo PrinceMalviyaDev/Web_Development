5e4b763b1e903e793404b7d83c8fab9b47d2ced3
PrinceMalviyaDev <pm306616@gmail.com>
2025-11-10 22:52:31 +0530

    delete tempcoderunner file

--- JS_Practice/Practice_12/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-t prom4 = Promise.allSettled([prom1, prom2]);
-
-// prom4.then((res) => {   // .then() will always print the status and the resolve/error message according to the settlement(resolved or rejected). It doesn't need the .catch() as it will never run.
-//     console.log(res);
-// });
