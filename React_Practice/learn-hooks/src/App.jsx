65daea5143e06575138443fde9e26f13900dc004
PrinceMalviyaDev <pm306616@gmail.com>
2026-02-25 19:39:59 +0530

    learn object updation in react

--- React_Practice/learn-hooks/src/App.jsx
+++ React_Practice/learn-hooks/src/App.jsx
@@ -1,5 +1,4 @@
-// React hook = Special function that allows functional components 
-//              to use React features without writing class components {React 16.8+}
+// React hook = Special function that allows functional components to use React features without writing class components {React 16.8+}
 //              (useState, useEffect, useContext, useReducer, useCallback, and more...)
 
 // useState() = A React hook that allows the creation of a stateful variable 
@@ -14,11 +13,13 @@
 import MyComponent from "./MyComponent.jsx";
 import Counter from "./Counter.jsx";
 import OnChange from "./OnChange.jsx";
+import ObjectUpdate from "./ObjectUpdate.jsx";
 
 function App() {
 
   return (
     <>
+      <ObjectUpdate/>
       <MyComponent/>
       <Counter/>
       <OnChange/>
