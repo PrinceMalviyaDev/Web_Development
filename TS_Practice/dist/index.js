d7ca49d1445887e2af4d3ec54f6b62d254975687
PrinceMalviyaDev <pm306616@gmail.com>
2026-03-08 00:44:16 +0530

    delete .js files from dist folder

--- TS_Practice/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-const userName = "World";
-console.log(greet(userName));
-//# sourceMappingURL=index.js.map
--- TS_Practice/dist/typesInTS.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=typesInTS.d.ts.map
