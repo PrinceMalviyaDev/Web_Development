b812feca2e8bebc68a74228f580facd11110bed5
PrinceMalviyaDev <pm306616@gmail.com>
2026-03-14 10:10:15 +0530

    complete TS

--- TS_Case_Studies/src/cs-17.ts
+++ TS_Case_Studies/src/cs-17.ts
@@ -8,7 +8,18 @@ class PhonePe implements PaymentGateway {
     processPayment(amount: number):Promise<boolean> {
         
         return new Promise((resolve) => {
-            console.log("Processing the amount: ", amount);
+            console.log("Processing the transaction using PhonePe: ", amount);
+            setTimeout(() => {
+                resolve(true);
+            }, 2000);
+        });
+    }
+}
+
+class GooglePay implements PaymentGateway {
+    processPayment (amount: number):Promise<boolean>{
+        return new Promise((resolve) => {
+            console.log(`Your transaction of amount ${amount} is being processed through GooglePay...`);
             setTimeout(() => {
                 resolve(true);
             }, 2000);
@@ -26,6 +37,7 @@ class Pay {
 (async function() {
     let pg: Pay = new Pay(new PhonePe());
     await pg.pay(5000) ? console.log("Payment Successful.") : console.log("Payment Failed!");
-})()
-
-
+    
+    pg = new Pay(new GooglePay());
+    await pg.pay(5000) ? console.log("Payment Successful.") : console.log("Payment Failed!");
+})();
